5b9bccb7719d1fe67495710d5fd822c846171bd5
0404233 <andrey0404233@gmail.com>
2024-05-01 16:28:09 +0300

    config:RSS-ECOMM-1-16 create jest tests script

--- src/.test.js
+++ src/.test.js
@@ -1,1 +1,8 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import App from './app/app.tsx';
+
+test('renders the app component', () => {
+  render(<App />);
+  // Add your test assertions here
+});
