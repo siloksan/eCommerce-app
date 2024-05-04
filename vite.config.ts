28e9fa3af799cfe76205b29a1eb6369068573a0c
siloksan <tutel07@gmail.com>
2024-05-04 13:54:21 +0300

    chore: RSS-ECOMM-1-36 configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: { 
+      components: '/src/components',
+      app: '/src/app',
+      pages: '/src/pages',
+      layout: '/src/layout',
+      utils: '/src/utils'
+    },
+  },
+});
