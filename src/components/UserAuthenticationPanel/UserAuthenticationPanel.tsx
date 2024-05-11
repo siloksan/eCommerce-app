910ff478490c2ab78daeed036d4b4265804f61ec
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2024-05-11 23:45:35 +0300

    feat:style header

--- src/components/UserAuthenticationPanel/UserAuthenticationPanel.tsx
+++ src/components/UserAuthenticationPanel/UserAuthenticationPanel.tsx
@@ -1,10 +1,11 @@
 import LinkButton from 'components/LinkButton/LinkButton';
+import styles from './UserAuthenticationPanel.module.scss';
 
 function UserAuthenticationPanel() {
   return (
-    <div>
-      <LinkButton text="Log in" link="#" />
-      <LinkButton text="Sign in" link="#" />
+    <div className={styles.panel}>
+      <LinkButton text="Log in" link="#" accent={false} />
+      <LinkButton text="Sign in" link="#" accent />
     </div>
   );
 }
