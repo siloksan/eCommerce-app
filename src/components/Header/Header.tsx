808186b64b0c19eab5f97fba63e449d446e63bb0
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2024-05-11 17:17:51 +0300

    feat: add logo to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import UserAuthenticationPanel from 'components/UserAuthenticationPanel/UserAuthenticationPanel';
+import Logo from 'components/Logo/Logo';
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Logo</h1>
+      <Logo />
       <UserAuthenticationPanel />
     </header>
   );
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,8 @@
 import logo from 'assets/icons/logo.svg';
+import styles from './Logo.module.scss';
 
 function Logo() {
-  return <img src={logo} alt="Coffee Lovers" />;
+  return <img src={logo} alt="Coffee Lovers" className={styles.logo} />;
 }
 
 export default Logo;
