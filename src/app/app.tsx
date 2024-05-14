060641e0f32579f841093efca1c34b832228a208
siloksan <tutel07@gmail.com>
2024-05-14 21:07:45 +0300

    config: set up vite-tsconfig-paths to automatically configure the path alias

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,12 @@
+import RegistrationPage from 'pages/Registration/Registration';
+
 function App() {
-  return <h1>eCommerce-App</h1>;
+  return (
+    <>
+      <h1>eCommerce-App</h1>
+      <RegistrationPage />
+    </>
+  );
 }
 
 export default App;
