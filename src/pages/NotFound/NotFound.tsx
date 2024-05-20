cb4c247e3b0c9237cc58bfe0dc5b36726c2a433c
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2024-05-20 04:03:13 +0300

    feat: add links for easier navigation

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage() {
-  return <h1>Not Found</h1>;
+  return (
+    <>
+      <h1>404</h1>
+      <h2>Oops! The page was Not Found</h2>
+      <Link to="/">Go to the main page</Link>
+    </>
+  );
 }
 
 export default NotFoundPage;
