2b8cc996d492952af491602a5c81f22a2a659a76
0404233 <andrey0404233@gmail.com>
2024-06-03 21:19:29 +0300

    feat: implement getting data from eCommerceTools and add loader

---
+++ src/shared/Loader/loader.tsx
@@ -0,0 +1,11 @@
+// import classes from './loader.module.scss';
+
+// function CustomLoader() {
+//   return (
+//     <div className={classes.cup}>
+//       <div className={classes.handle}></div>
+//     </div>
+//   );
+// }
+
+// export default CustomLoader();
